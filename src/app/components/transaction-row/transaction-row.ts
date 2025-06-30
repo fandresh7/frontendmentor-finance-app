a03beb74b3d24dc56280de27aa251e08c3c859ac
Andres Hortua <fandresh7@gmail.com>
2025-06-30 12:16:10 -0500

    feat: implement responsive transaction list with new TransactionRow component and update currency pipe format

---
+++ src/app/components/transaction-row/transaction-row.ts
@@ -0,0 +1,20 @@
+import { CurrencyPipe, DatePipe } from '@angular/common'
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core'
+
+@Component({
+  selector: 'tr[transaction-row]', // Selecciona elementos tr con el atributo transaction-row
+  standalone: true,
+  imports: [CurrencyPipe, DatePipe],
+  templateUrl: './transaction-row.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TransactionRow {
+  avatar = input.required<string>()
+  name = input.required<string>()
+  amount = input.required<number>()
+  date = input.required<string>()
+  category = input.required<string>()
+
+  color = computed(() => (this.amount() > 0 ? 'text-green' : 'text-grey-900'))
+  sign = computed(() => (this.amount() > 0 ? '+' : ''))
+}
--- src/app/pages/transactions/transactions.ts
+++ src/app/pages/transactions/transactions.ts
@@ -1,10 +1,51 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
+
+import { TransactionRow } from '@components/transaction-row/transaction-row'
 import { Pagination } from '@shared/components/pagination/pagination'
+import { Card } from '@shared/components/card/card'
 
 @Component({
   selector: 'transactions',
-  imports: [Pagination],
+  imports: [Pagination, Card, TransactionRow],
   templateUrl: './transactions.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Transactions {}
+export class Transactions {
+  transactions = [
+    {
+      avatar: './images/avatars/emma-richardson.jpg',
+      name: 'Emma Richardson',
+      amount: 75.5,
+      category: 'Personal Care',
+      date: '2024-08-19T14:23:11Z'
+    },
+    {
+      avatar: './images/avatars/savory-bites-bistro.jpg',
+      name: 'Savory Bites Bistro',
+      amount: -55.5,
+      category: 'Food',
+      date: '2024-08-19T20:23:11Z'
+    },
+    {
+      avatar: './images/avatars/daniel-carter.jpg',
+      name: 'Daniel Carter',
+      amount: -42.3,
+      category: 'Personal Care',
+      date: '2024-08-18T09:45:32Z'
+    },
+    {
+      avatar: './images/avatars/sun-park.jpg',
+      name: 'Sun Park',
+      amount: 120,
+      category: 'Food',
+      date: '2024-08-17T16:12:05Z'
+    },
+    {
+      avatar: './images/avatars/urban-services-hub.jpg',
+      name: 'Urban Services Hub',
+      amount: -65,
+      category: 'Utilities',
+      date: '2024-08-17T21:08:09Z'
+    }
+  ]
+}
