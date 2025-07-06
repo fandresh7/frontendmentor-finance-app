81dad9b2bc735f8bd72377fc7946f54564362d44
Andres Hortua <fandresh7@gmail.com>
2025-07-06 13:47:39 -0600

    feat: implement filter functionality with dynamic categories and sort options in transactions component

--- src/app/components/transactions-filters/transactions-filters.ts
+++ src/app/components/transactions-filters/transactions-filters.ts
@@ -1,15 +1,23 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core'
+import { ChangeDetectionStrategy, Component, input, model, signal } from '@angular/core'
 import { OverlayModule } from '@angular/cdk/overlay'
+import { FormsModule } from '@angular/forms'
 
 import { SearchIcon, ChevronDownIcon, FilterIcon, DownloadIcon } from '@shared/components/icons/icons'
 
 @Component({
   selector: 'transactions-filters',
-  imports: [SearchIcon, ChevronDownIcon, OverlayModule, FilterIcon, DownloadIcon],
+  imports: [SearchIcon, ChevronDownIcon, FilterIcon, DownloadIcon, OverlayModule, FormsModule],
   templateUrl: './transactions-filters.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TransactionsFilters {
+  categories = input<string[]>([])
+  sortOptions = input<string[]>([])
+
+  search = model<string>('')
+  selectedCategory = model<string | null>(null)
+  selectedSortOption = model<string | null>(null)
+
   isSortByOpen = signal<boolean>(false)
   isCategoryOpen = signal<boolean>(false)
 
@@ -27,5 +35,15 @@ export class TransactionsFilters {
 
   closeCategory() {
     this.isCategoryOpen.set(false)
+  }
+
+  selectCategory(category: string) {
+    this.selectedCategory.set(category)
+    this.closeCategory()
+  }
+
+  selectSortOption(option: string) {
+    this.selectedSortOption.set(option)
+    this.closeSortBy()
   }
 }
