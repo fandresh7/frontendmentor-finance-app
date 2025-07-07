57d9b25ec964353060409081e5e93d003e80f1f7
Andres Hortua <fandresh7@gmail.com>
2025-07-06 20:38:42 -0600

    feat: add dropdown menu to budget summary card with edit and delete actions

--- src/app/components/budget-summary-card/budget-summary-card.ts
+++ src/app/components/budget-summary-card/budget-summary-card.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core'
+import { OverlayModule } from '@angular/cdk/overlay'
+
 import { Card } from '@shared/components/card/card'
 import { EllipsisIcon } from '@shared/components/icons/icons'
 import { ProgressBar } from '@shared/components/progress-bar/progress-bar'
@@ -8,8 +10,22 @@ import { TransactionItem } from '../transaction-item/transaction-item'
 
 @Component({
   selector: 'budget-summary-card',
-  imports: [Card, EllipsisIcon, ProgressBar, CategoryItem, CardHeader, TransactionItem],
+  imports: [Card, EllipsisIcon, ProgressBar, CategoryItem, CardHeader, TransactionItem, OverlayModule],
   templateUrl: './budget-summary-card.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BudgetSummaryCard {}
+export class BudgetSummaryCard {
+  isOpen = signal(false)
+
+  toggleDropdown() {
+    this.isOpen.set(!this.isOpen())
+  }
+
+  closeDropdown() {
+    this.isOpen.set(false)
+  }
+
+  openModal() {
+    console.log('openModal')
+  }
+}
