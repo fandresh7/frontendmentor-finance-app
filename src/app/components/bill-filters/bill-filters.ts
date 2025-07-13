0b740dca94c29840f80f4ed303d6a28281d0203e
Andres Hortua <fandresh7@gmail.com>
2025-07-13 16:48:08 -0600

    feat: implement bill filters component with search and sort functionality

--- src/app/components/bill-filters/bill-filters.ts
+++ src/app/components/bill-filters/bill-filters.ts
@@ -1,9 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, input, model, signal } from '@angular/core'
+import { OverlayModule } from '@angular/cdk/overlay'
+import { FormsModule } from '@angular/forms'
+
+import { SearchIcon, ChevronDownIcon, DownloadIcon } from '@shared/components/icons/icons'
 
 @Component({
   selector: 'bill-filters',
-  imports: [],
+  imports: [OverlayModule, FormsModule, SearchIcon, ChevronDownIcon, DownloadIcon],
   templateUrl: './bill-filters.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BillFilters {}
+export class BillFilters {
+  sortOptions = input<string[]>([])
+
+  search = model<string>('')
+  selectedSortOption = model<string | null>(null)
+
+  isSortByOpen = signal<boolean>(false)
+
+  toggleSortBy() {
+    this.isSortByOpen.set(!this.isSortByOpen())
+  }
+
+  closeSortBy() {
+    this.isSortByOpen.set(false)
+  }
+
+  selectSortOption(option: string) {
+    this.selectedSortOption.set(option)
+    this.closeSortBy()
+  }
+}
