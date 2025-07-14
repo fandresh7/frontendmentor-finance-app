7ac166c770e6c93f598b7c3a6841c3de95cf981c
Andres Hortua <fandresh7@gmail.com>
2025-07-13 19:16:51 -0600

    feat: implement login page UI with email/password form and authentication illustration

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { Card } from '@shared/components/card/card'
+import { EyeIcon, EyeOffIcon } from '@shared/components/icons/icons'
 
 @Component({
   selector: 'login',
-  imports: [],
+  imports: [Card, EyeIcon, EyeOffIcon],
   templateUrl: './login.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
