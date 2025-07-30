8272a7cdfee20556e4e32cd64019c1c5499f4b95
Andres Hortua <fandresh7@gmail.com>
2025-07-29 21:21:36 -0600

    feat: implement signup page UI and add navigation between auth pages

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
 import { Card } from '@shared/components/card/card'
 import { EyeIcon, EyeOffIcon } from '@shared/components/icons/icons'
 
 @Component({
   selector: 'login',
-  imports: [Card, EyeIcon, EyeOffIcon],
+  imports: [Card, EyeIcon, EyeOffIcon, RouterLink],
   templateUrl: './login.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -1,9 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { EyeIcon, EyeOffIcon } from '@shared/components/icons/icons'
+import { Card } from '@shared/components/card/card'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'signup',
-  imports: [],
+  imports: [EyeIcon, EyeOffIcon, Card, RouterLink],
   templateUrl: './signup.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'w-full block'
+  }
 })
 export class Signup {}
