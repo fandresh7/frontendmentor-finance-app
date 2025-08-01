ac471ffd00f5841afc573fcee4d866bd25bcb7c6
Andres Hortua <fandresh7@gmail.com>
2025-07-31 19:47:06 -0600

    feat: add form validation and reactive forms to login page and clean up signup form

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 import { RouterLink } from '@angular/router'
 import { Card } from '@shared/components/card/card'
 import { InputPassword } from '@shared/forms/components/input-password/input-password'
@@ -6,11 +7,23 @@ import { InputText } from '@shared/forms/components/input-text/input-text'
 
 @Component({
   selector: 'login',
-  imports: [Card, RouterLink, InputPassword, InputText],
+  imports: [Card, RouterLink, InputPassword, InputText, ReactiveFormsModule],
   templateUrl: './login.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     class: 'w-full block'
   }
 })
-export class Login {}
+export class Login {
+  private fb = inject(FormBuilder)
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
+
+  onSubmit(): void {
+    console.log('Form submitted:', this.loginForm.value)
+    this.loginForm.markAllAsTouched()
+  }
+}
--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -4,7 +4,6 @@ import { Card } from '@shared/components/card/card'
 import { RouterLink } from '@angular/router'
 import { InputPassword } from '@shared/forms/components/input-password/input-password'
 import { InputText } from '@shared/forms/components/input-text/input-text'
-import { validateNameAsync } from '@shared/forms/validators/name-validators'
 
 @Component({
   selector: 'signup',
@@ -19,19 +18,12 @@ export class Signup {
   private fb = inject(FormBuilder)
 
   signupForm = this.fb.group({
-    name: ['', [Validators.required], [validateNameAsync]],
+    name: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(8)]]
   })
 
-  constructor() {
-    this.signupForm.get('name')?.statusChanges.subscribe(status => {
-      console.log({ status })
-    })
-  }
-
   onSubmit(): void {
-    console.log('sdfdsf')
     console.log('Form submitted:', this.signupForm.value)
     this.signupForm.markAllAsTouched()
   }
